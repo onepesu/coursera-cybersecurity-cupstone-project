a0d46ab7153fbd4d001a334811021404bebc2821
Stephanos Papanikolopoulos <spapanik21@gmail.com>
2015-05-09 20:58:10 +0100

    checking for missing arguments

--- build/logparser.py
+++ build/logparser.py
@@ -11,15 +11,20 @@ def parse_args(argument_list, mapping):
                 key = mapping[argument]['name']
                 parsed_arguments[key] = True
                 accepting_arguments = False
-            else:
+            elif accepting_arguments is False:
                 key = mapping[argument]['name']
                 if mapping[argument].get('max_args') is not None:
                     if mapping[argument]['max_args'] == 0:
                         raise ValidationError('Too many {}'.format(argument))
                     mapping[argument]['max_args'] -= 1
                 accepting_arguments = True
+            else:
+                raise ValidationError('Missing argument')
         elif accepting_arguments:
-            parsed_arguments[key] = argument
+            try:
+                parsed_arguments[key].append(argument)
+            except KeyError:
+                parsed_arguments[key] = [argument]
             accepting_arguments = False
         else:
             raise ValidationError('Unexpected argument')
