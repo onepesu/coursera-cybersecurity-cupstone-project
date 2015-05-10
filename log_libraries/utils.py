15e2632b17ee7eb34523ec4dfe6f9227aad36581
Martha Giannoudovardi <maapxa@gmail.com>
2015-05-10 17:55:35 +0100

    extract method

--- log_libraries/utils.py
+++ log_libraries/utils.py
@@ -17,10 +17,23 @@ def append_to_log(arguments, filename):
     lines = result.strip().split()[0]
     encrypt = Encrypt(arguments['token'][0], lines)
     type_ = 'A' if arguments.get('arrival') else 'D'
-    room_id = arguments.get('room_id') or 'G'
+    room_id = arguments.get('room_id', [None])[0] or 'G'
     plaintext_arguments = json.dumps([
         arguments['timestamp'][0], arguments.get('employee', [''])[0],
         arguments.get('guest', [''])[0], type_, room_id
     ])
     with open(filename, 'a') as opened_file:
         opened_file.write(encrypt.encrypt(plaintext_arguments) + '\n')
+
+
+def extract(arguments, filename):
+    out = []
+    with open(filename, 'r') as opened_file:
+        for n, line in enumerate(opened_file.readlines()):
+            if n == 0:
+                continue
+            decryptor = Encrypt(arguments['token'][0], n)
+            decrypted_line = decryptor.decrypt(line.replace('\n', ''))
+            out.append(json.loads(decrypted_line))
+    return out
+
