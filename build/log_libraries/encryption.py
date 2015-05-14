1a3e43ffb45dd0c71291ead352a8a27d428e4c9f
Stephanos Papanikolopoulos <spapanik21@gmail.com>
2015-05-14 21:53:13 +0100

    encryption_test_time

--- build/log_libraries/encryption.py
+++ build/log_libraries/encryption.py
@@ -1,11 +1,31 @@
+import base64
+import hashlib
+from Crypto.Cipher import AES
+from Crypto import Random
+
+BS = 32
+
+
+def pad(s):
+    return s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+
+
+def unpad(s):
+    return s[:-ord(s[len(s)-1:])]
+
+
 class Encrypt(object):
-    def __init__(self, token):
-        self.token = token
+    def __init__(self, key):
+        self.key = hashlib.md5(key).hexdigest()
 
-    def encrypt(self, message):
-        encrypted_message = message + self.token
-        return encrypted_message
+    def encrypt(self, raw):
+        raw = pad(raw)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(raw))
 
-    def decrypt(self, encrypted_message):
-        message = encrypted_message[:-len(self.token)]
-        return message
+    def decrypt(self, enc):
+        enc = base64.b64decode(enc)
+        iv = enc[:16]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return unpad(cipher.decrypt(enc[16:]))
