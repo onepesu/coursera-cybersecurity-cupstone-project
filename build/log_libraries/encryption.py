1de2d677ca55cbefc4a6481f867fb80b0d92baf6
Stephanos Papanikolopoulos <spapanik21@gmail.com>
2015-05-20 21:54:41 +0100

    even less space

--- build/log_libraries/encryption.py
+++ build/log_libraries/encryption.py
@@ -1,5 +1,5 @@
 import json
-import zlib
+import bz2
 import hashlib
 from Crypto import Random
 from Crypto.Cipher import AES
@@ -22,7 +22,7 @@ class Encrypt(object):
 
     def encrypt(self, raw):
         json_obj = json.dumps(raw).replace(' ', '')
-        raw = pad(zlib.compress(json_obj))
+        raw = pad(bz2.compress(json_obj))
         iv = Random.new().read(AES.block_size)
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         to_write = iv + cipher.encrypt(raw)
@@ -32,7 +32,7 @@ class Encrypt(object):
         iv = enc[:16]
         cipher = AES.new(self.key, AES.MODE_CBC, iv)
         try:
-            unpadded = zlib.decompress(unpad(cipher.decrypt(enc[16:])))
-        except zlib.error:
+            unpadded = bz2.decompress(unpad(cipher.decrypt(enc[16:])))
+        except IOError:
             raise ValueError('bad token')
         return json.loads(unpadded)
